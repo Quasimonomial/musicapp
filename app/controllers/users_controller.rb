eaf3c52b910db37222bb098aedafc5f8e0ff031d
Travis Herrick <quasimonomial@gmail.com>
2014-10-30 13:37:27 -0700

    Added application controller helper methods, fixed a bug in routes.rb, wrote a users's controller that lets us create users, passwords not yet implimented

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,27 @@
 class UsersController < ApplicationController
+  def index
+    render json: User.all
+  end
+
+  def show
+    render json: User.find(user_params[:id])
+  end
+
   def create
-    #stuff
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to user_url(@user.id)
+    else
+      render json: @user.errors.full_messages
+    end
   end
 
   def new
-    #stuff
+    User.new
   end
-
+  
+  private
+  def user_params
+    params.require(:user).permit(:email, :password_digest)
+  end
 end
