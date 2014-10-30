b2812032bec047368f7c345a1c007b2e9526520e
Travis Herrick <quasimonomial@gmail.com>
2014-10-30 13:58:33 -0700

    Got rid of unessesary routes for users and session

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
-  resources :users
+  resources :users, :only => [:new, :create]
 
-  resource :session
+  resource :session, :only => [:new, :create, :destroy]
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
