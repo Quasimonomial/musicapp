9da14f9211f1ce309b4e11203a6247af58248a62
Travis Herrick <quasimonomial@gmail.com>
2014-10-30 01:24:28 -0700

    User Model is complete at least for now, can hash passwords and properly sets a session token upon initialization

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,4 +11,19 @@ class User <  ActiveRecord::Base
   def ensure_session_token
     self.session_token ||= SecureRandom.urlsafe_base64
   end
+
+  def reset_session_token!
+    self.session_token = SecureRandom.urlsafe_base64
+    self.save!
+    self.session_token
+  end
+
+  def password= password
+    password == BCrypt::Password.create(self.password_digest)
+
+  end
+
+  def is_password? password
+    BCrypt::Password.new(self.password_digest)
+  end
 end
